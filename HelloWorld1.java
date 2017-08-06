92cbe16850b5c40904a2843577b43c5cb799cd46
CindieDuong <30774570+CindieDuong@users.noreply.github.com>
2017-08-07 00:23:03 +1000

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,15 @@
+/**
+*T his is the exercise 1 - Hello World 1
+* The name of class is HellloWorld.
+  The system displays the string "Hello Chau!" as the output of the main method.
+  This is the specific string which entered in coding process
+*
+* @author       Chau Duong
+* @version      1.0
+* @Date         04-08-2016
+*/
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Chau!"); // Display the string.
+    }
+}
